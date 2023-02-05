21f5b69ec36329b625df3735336f70dddec619c3
0AliReza0 <alireza.abedi9310@gmail.com>
2023-02-05 23:36:14 +0330

    vazir font added...

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
-import MainLayout from "@/layouts/MainLayout";
+import '@/assets/css/style.css'
 import "@/styles/globals.css";
+import MainLayout from "@/layouts/MainLayout";
 
 export default function App({ Component, pageProps }) {
   return (
