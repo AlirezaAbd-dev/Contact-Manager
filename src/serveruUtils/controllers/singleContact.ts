c5d237e1e64dd00f3a74014b58d5aa30212651d7
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-11 15:13:41 +0330

    route names edited...

---
+++ src/serveruUtils/controllers/singleContact.ts
@@ -0,0 +1,6 @@
+import { NextApiResponse } from "next";
+import { CustomAddContactRequest } from "../../../types";
+
+const singleContact = async (req: CustomAddContactRequest, res:NextApiResponse) => {
+
+}
