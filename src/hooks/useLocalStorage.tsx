4f7cbc11f665b3a82961fb8a7318e1bd09a86417
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-11 21:42:47 +0330

    useLocalStorage hook created...

---
+++ src/hooks/useLocalStorage.tsx
@@ -0,0 +1,19 @@
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function useLocalStorage(tokenName: string) {
+  const [token, setToken] = useState<string>();
+
+  const router = useRouter();
+
+  useEffect(() => {
+    const localtoken = localStorage.getItem(tokenName);
+    if (localtoken) {
+      setToken(localtoken);
+    } else {
+      router.replace("/login");
+    }
+  }, [localStorage.getItem(tokenName)]);
+
+  return token;
+}
