1da348495cb87c9d438c79c0e0b51d74aaea859d
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-20 20:03:41 +0330

    bug fixed...

--- src/serveruUtils/controllers/deleteContact.ts
+++ src/serveruUtils/controllers/deleteContact.ts
@@ -1,8 +1,10 @@
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { ObjectId } from "mongodb";
 import { NextApiResponse } from "next";
 import { CustomAddContactRequest } from "../../../types";
 import userCollection from "../collection/userCollection";
 import client from "../databaseClient/client";
+import arvanCloudConnection from "../helpers/arvanCloudConnection";
 import verifyToken from "../middleware/verifyToken";
 
 const deleteContact = async (
@@ -30,6 +32,33 @@ const deleteContact = async (
   }
 
   const userEmail = verifiedUser.email;
+
+  const user = await userCollection.findOne({ email: userEmail });
+
+  let userImageURL: string | undefined = "";
+  if (user) {
+    userImageURL = user.contacts.find(
+      (contact) => contact._id.toString() === id
+    )?.image;
+  } else {
+    await client.close();
+    return res.status(404).send({ message: "مخاطب مورد نظر یافت نشد!" });
+  }
+
+  if (userImageURL) {
+    const s3 = arvanCloudConnection;
+    const imageKey = userImageURL.split("/")[3];
+    try {
+      await s3.send(
+        new DeleteObjectCommand({
+          Bucket: process.env.ARVAN_BUCKET_NAME!,
+          Key: imageKey,
+        })
+      );
+    } catch (err) {
+      console.log("Error", err);
+    }
+  }
 
   // Delete The Chosen Cintact From Contacts In Database With $pull Query
   try {
