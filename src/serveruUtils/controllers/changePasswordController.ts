4310527987898f824af706911d1c36d99d7cb9f8
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-22 17:59:06 +0430

    change password completed...

--- src/serveruUtils/controllers/changePasswordController.ts
+++ src/serveruUtils/controllers/changePasswordController.ts
@@ -1,4 +1,6 @@
 import { NextApiResponse } from "next";
+import bcrypt from "bcrypt";
+
 import { ChangePasswordRequest } from "../../pages/api/password";
 import userCollection from "../collection/userCollection";
 import client from "../databaseClient/client";
@@ -45,15 +47,30 @@ const handler = async (req: ChangePasswordRequest, res: NextApiResponse) => {
     return res.status(500).json({ message: "تغییر رمز عبور با خطا مواجه شد!" });
   }
 
+  let hashedPassword: string = "";
   try {
-    await userCollection.updateOne(
-      { email: verifiedUser?.email },
-      {
-        $set: {
-          password: newPassword,
-        },
-      }
-    );
+    hashedPassword = await bcrypt.hash(newPassword, +process.env.SALT!);
+  } catch (err) {
+    await client.close();
+    console.log(err);
+    return res.status(500).json({ message: "خطایی در سرور رخ داد!" });
+  }
+
+  try {
+    if (hashedPassword !== "" || !hashedPassword) {
+      await userCollection.updateOne(
+        { email: verifiedUser?.email },
+        {
+          $set: {
+            password: hashedPassword,
+          },
+        }
+      );
+
+      return res
+        .status(200)
+        .json({ message: "رمز عبور شما با موفقیت تغییر پیدا کرد" });
+    }
   } catch (err) {
     await client.close();
     console.log(err);
