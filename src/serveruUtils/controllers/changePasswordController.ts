cc3ee844dcf1e64465acce8d70b7f27edfca32f1
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-31 13:44:25 +0430

    changePassword uses mongoose instead of mongodb

--- src/serveruUtils/controllers/changePasswordController.ts
+++ src/serveruUtils/controllers/changePasswordController.ts
@@ -2,13 +2,10 @@ import { NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 
 import { ChangePasswordRequest } from "../../pages/api/password";
-import userCollection, {
-  UserCollectiontype,
-} from "../collection/userCollection";
-import client from "../databaseClient/client";
 import verifyResetPasswordToken from "../middleware/verifyResetPasswordToken";
 import changePasswordValidation from "../validations/changePasswordValidation";
-import { WithId } from "mongodb";
+import dbConnect from "../database/dbConnect";
+import UserModel from "../models/userModel";
 
 const handler = async (req: ChangePasswordRequest, res: NextApiResponse) => {
   const verifiedUser = verifyResetPasswordToken(req);
@@ -25,61 +22,31 @@ const handler = async (req: ChangePasswordRequest, res: NextApiResponse) => {
 
   const { password: newPassword } = req.body;
 
-  try {
-    await client.connect();
-  } catch (err) {
-    await client.close();
-    console.log(err);
-    return res
-      .status(500)
-      .json({ message: "اتصال با دیتابیس با خطا مواجه شد!" });
-  }
+  await dbConnect();
 
-  let findUser: WithId<UserCollectiontype> | null;
-  try {
-    findUser = await userCollection.findOne({
-      email: verifiedUser?.email,
-    });
+  const findUser = await UserModel.findOne({ email: verifiedUser.email });
 
-    if (!findUser?._id) {
-      await client.close();
-      return res.status(404).json({ message: "کاربر مورد نظر یافت نشد!" });
-    }
-  } catch (err) {
-    await client.close();
-    console.log(err);
-    return res.status(500).json({ message: "تغییر رمز عبور با خطا مواجه شد!" });
+  if (!findUser || !findUser?._id) {
+    return res.status(404).json({ message: "کاربر مورد نظر یافت نشد!" });
   }
 
   let hashedPassword: string = "";
   try {
     hashedPassword = await bcrypt.hash(newPassword, +process.env.SALT!);
   } catch (err) {
-    await client.close();
     console.log(err);
     return res.status(500).json({ message: "خطایی در سرور رخ داد!" });
   }
 
-  try {
-    if (hashedPassword !== "" || !hashedPassword) {
-      await userCollection.updateOne(
-        { email: verifiedUser?.email },
-        {
-          $set: {
-            resetPassAmount: +findUser.resetPassAmount++,
-            password: hashedPassword,
-          },
-        }
-      );
+  if (hashedPassword !== "") {
+    findUser.resetPassAmount += 1;
+    findUser.password = hashedPassword;
 
-      return res
-        .status(200)
-        .json({ message: "رمز عبور شما با موفقیت تغییر پیدا کرد" });
-    }
-  } catch (err) {
-    await client.close();
-    console.log(err);
-    return res.status(500).json({ message: "تغییر رمز عبور با خطا مواجه شد!" });
+    const result = await findUser.save();
+
+    return res
+      .status(200)
+      .json({ message: "رمز عبور شما با موفقیت تغییر پیدا کرد", result });
   }
 };
 
