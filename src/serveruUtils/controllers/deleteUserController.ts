2e31fc786c7b178a55d70906ec51e92041534402
0AliReza0 <alireza.abedi9310@gmail.com>
2023-03-31 14:19:32 +0430

    delete contact uses mongoose instead of mongodb

--- src/serveruUtils/controllers/deleteUserController.ts
+++ src/serveruUtils/controllers/deleteUserController.ts
@@ -2,22 +2,16 @@ import { NextApiResponse } from "next";
 import bcrypt from "bcrypt";
 
 import { CustomDeleteRequest } from "../../pages/api/account";
-import userCollection from "../collection/userCollection";
-import client from "../databaseClient/client";
 import signInValidation from "../validations/signInValidation";
+import dbConnect from "../database/dbConnect";
+import UserModel, { UserModelType } from "../models/userModel";
 
 const deleteAccountController = async (
   req: CustomDeleteRequest,
   res: NextApiResponse
 ) => {
   // Database Connection
-  try {
-    await client.connect();
-  } catch (err) {
-    return res
-      .status(500)
-      .send({ message: "اتصال با دیتابیس با خطا مواجه شد!" });
-  }
+  await dbConnect();
 
   // Validating Request Body
   const validatedBody = signInValidation.safeParse(req.body);
@@ -28,10 +22,11 @@ const deleteAccountController = async (
       .json({ message: validatedBody.error.issues[0].message });
   }
 
-  const findUser = await userCollection.findOne({ email: req.body.email });
+  const findUser = await UserModel.findOne<UserModelType>({
+    email: req.body.email,
+  });
 
   if (!findUser) {
-    await client.close();
     return res
       .status(403)
       .json({ message: "نام کاربری یا رمز عبور اشتباه است!" });
@@ -46,15 +41,9 @@ const deleteAccountController = async (
     return res.json({ message: "نام کاربری یا رمز عبور اشتباه است!" });
   }
 
-  const result = await userCollection.findOneAndDelete({
+  await UserModel.deleteOne({
     email: req.body.email,
   });
-
-  await client.close();
-
-  if (!result.ok) {
-    return res.status(500).json({ message: "خطایی در سرور رخ داد!" });
-  }
 
   return res.status(200).json({ message: "حساب شما با موفقیت حذف شد!" });
 };
